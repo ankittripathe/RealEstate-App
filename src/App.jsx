2d55ccc2b76d1073250c6eb1ec022d95b7af9a36
Dhoni <ankitdhonee@gmail.com>
2025-03-31 14:43:03 +0545

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import About from "./Components/About";
+import Project from "./Components/Project";
 
 function App() {
   return (
     <div className="w-full overflow-hidden" >
       <Header />
       <About/>
+      <Project/>
     </div>
   );
 }
---
+++ src/Components/Project.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { assetsCollection, projectsData } from "../assets/assets";
+
+const Project = () => {
+  return (
+    <div
+      className=" container mx-auto py-4 px-6 md:px-20 lg:px-32 my-16 w-full overflow-hidden"
+      id="Projects"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Projects{" "}
+        <span className="underline underline-offset-4 decoration-1 font-light">
+          Colmpleted
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+      </p>
+
+      {/* Slider buttons */}
+      <div className="flex justify-end items-center mb-8">
+        <button
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Previous Project"
+        >
+          <img src={assetsCollection.left_arrow} alt="PreviousIcon" />
+        </button>
+
+        <button
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Next Project"
+        >
+          <img src={assetsCollection.right_arrow} alt="NextIcon" />
+        </button>
+      </div>
+
+      {/* Project slider container */}
+      <div className="overflow-hidden">
+        <div className="flex gap-6 transition-transform duration-500 ease-in-out">
+          {projectsData.map((project, index) => {
+            return (
+              <>
+                <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+                  <img src={project.image} alt={project.title} className="w-full h-auto mb-14"/>
+                <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                  <div className="inline-block bg-white w-3/4 px-4 py-1 shadow-md">
+                   <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                   </h2>
+                   <p className="text-gray-500 text-sm">
+                    {project.price} <span>{project.location}</span>
+                   </p>
+                  </div>
+                </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
