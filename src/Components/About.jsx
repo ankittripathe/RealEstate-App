272bd48b013f49e01f53ec6c24e86c137cb2403c
Dhoni <ankitdhonee@gmail.com>
2025-03-31 12:31:27 +0545

    about done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,39 +3,39 @@ import { assetsCollection } from "../assets/assets";
 
 const About = () => {
   return (
+    // full main div
     <div
-      className="flex flex-col justify-center items-center container mx-auto w-full p-14 md:p-20 lg:p-32  overflow-hidden"
+      className=" flex flex-col items-center justify-center container mx-auto p-14 md:p20 lg:p-32 w-full overflow-hidden"
       id="About"
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2">
-        About
+        About{" "}
         <span className="underline underline-offset-4 decoration-1 under font-light">
           Our Brand
         </span>
       </h1>
-
       <p className="text-gray-500 max-w-80 text-center mb-8">
-        Passionate About Properties, Dedicated to Your Vision
+        Passionate About Proprties, Dedicated to Your Vision
       </p>
 
-      {/* Image */}
+      {/* 2nd main div */}
       <div className="flex flex-col items-center md:flex-row md:items-start md:gap-20">
         <img
           src={assetsCollection.brand_img}
-          alt=""
           className="w-full sm:w-1/2 max-w-lg"
+          alt=""
         />
-
-        <div className="bg-amber-300 flex flex-col items-center">
-          <div>
-            <div>
+        
+        <div className="flex flex-col items-center  md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div> 
               <p className="text-4xl font-medium text-gray-800">10+</p>
               <p>Years of Excellence</p>
             </div>
 
             <div>
               <p className="text-4xl font-medium text-gray-800">12+</p>
-              <p>Projects Completed</p>
+              <p>Project Completed</p>
             </div>
 
             <div>
@@ -48,7 +48,12 @@ const About = () => {
               <p>Ongoing Projects</p>
             </div>
           </div>
-        </div>
+          <p className="my-6 max-w-lg">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, labore ex quis quas nulla nesciunt. Dolore consequuntur soluta veniam sunt nostrum optio in, expedita dolores facilis rem 
+          </p>
+          <button className="bg-blue-600 text-white rounded px-8 py-2">Learn More</button>
+
+        </div> 
       </div>
     </div>
   );
